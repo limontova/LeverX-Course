0e647c421deba54ec98ae270fb7371e2f4b4e389
limontova <62797799+limontova@users.noreply.github.com>
2021-10-22 16:05:46 +0300

    added 3, 4 tasks

---
+++ Task_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Dictionary<char, int> arrayOfChars = new Dictionary<char, int>();
+            string result = string.Empty;
+
+            foreach (var symbol in input)
+            {
+                if (!arrayOfChars.ContainsKey(symbol))
+                {
+                    arrayOfChars.Add(symbol, 1);
+                }
+                else
+                {
+                    arrayOfChars[symbol]++;
+                }
+                result += arrayOfChars[symbol];
+            }
+        }
+    }
+}
---
+++ Task_3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_3
+{
+    class Program
+    {
+        static List<T> UniqueInOrder<T>(IEnumerable<T> sequence) where T : IEquatable<T>
+        {
+            var result = new List<T>();
+            T previousSymbol = sequence.GetEnumerator().Current;
+            bool was = false;
+            foreach (var element in sequence)
+            {
+                if (was)
+                {
+                    if (!previousSymbol.Equals(element)) 
+                    {
+                        result.Add(element);
+                        previousSymbol = element; 
+                    }
+                }
+                else
+                {
+                    was = true;
+                    result.Add(element);
+                }
+            }
+                return result;
+        }
+        static void Main(string[] args)
+        {
+            IEnumerable<int> numbers = new List<int>() { 1, 2, 3, 3, 4, 4 , 4, 4 , 5};
+
+            UniqueInOrder(numbers);
+        }
+    }
+}
