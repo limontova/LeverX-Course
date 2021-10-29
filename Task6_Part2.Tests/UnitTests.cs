6aea8dcebc68f9c3be06054a1c196ed5a70bfc77
limontova <62797799+limontova@users.noreply.github.com>
2021-10-29 15:41:28 +0300

    corrected tests

--- Task6_Part2.Tests/UnitTests.cs
+++ Task6_Part2.Tests/UnitTests.cs
@@ -11,34 +11,6 @@ namespace Task6_Part2.Tests
         {
         }
 
-        [Test]
-        public void CountPriceOfAllBooks_BuyAllBooks_ShouldReturnCost25Discount()
-        {
-            //Arrange
-            int copiesOfFirstBook = 2;
-            int copiesOfSecondBook = 2;
-            int copiesOfThirdBook = 2;
-            int copiesOfFourthBook = 1;
-            int copiesOfFifthBook = 1;
-
-            //Act
-            double actual = BookShop.CountPriceOfAllBooks(copiesOfFirstBook, copiesOfSecondBook, copiesOfThirdBook, copiesOfFourthBook, copiesOfFifthBook);
-
-            //Assert
-            Assert.AreEqual(51.6, actual);
-        }
-
-        [TestCase(0, 2, 1, 1, 5)]
-        [TestCase(2, 1, 0, 5, 1)]
-        public void CountPriceOfAllBooks_BuyAllBooks_ShouldReturnCost25Discount(int copiesOfFirstBook, int copiesOfSecondBook, int copiesOfThirdBook, int copiesOfFourthBook, int copiesOfFifthBook)
-        {
-            //Act
-            double actual = BookShop.CountPriceOfAllBooks(copiesOfFirstBook, copiesOfSecondBook, copiesOfThirdBook, copiesOfFourthBook, copiesOfFifthBook);
-
-            //Assert
-            Assert.AreEqual(64.8, actual);
-        }
-
         public static IEnumerable<TestCaseData> NumberOfBooksProvider()
         {
             yield return new TestCaseData(1, 0, 0, 0, 0, 8);
@@ -46,6 +18,8 @@ namespace Task6_Part2.Tests
             yield return new TestCaseData(1, 2, 6, 0, 0, 68.8);
             yield return new TestCaseData(2, 2, 3, 1, 0, 55.2);
             yield return new TestCaseData(3, 2, 5, 1, 2, 86.8);
+            yield return new TestCaseData(0, 1, 2, 1, 5, 64.8);
+            yield return new TestCaseData(2, 2, 2, 1, 1, 51.6);
         }
 
         [Test, TestCaseSource("NumberOfBooksProvider")]
