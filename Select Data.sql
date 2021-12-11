42efc3b9efd6b071a19c17baff72cc6154ded4a0
limontova <62797799+limontova@users.noreply.github.com>
2021-12-11 16:46:53 +0300

    add Select_Data script

--- Select Data.sql
+++ Select Data.sql
@@ -37,3 +37,40 @@ WHERE (Birthday >= '1990-01-01' AND Birthday <= '1991-01-01') ORDER BY Bursary D
 SELECT Student.Name, Student.GroupID, Student.Bursary/max(Student.Bursary) OVER () as [BursaryPercentage] from Student
 WHERE (GroupID = 102)
 group by Student.Name, Student.GroupID, Bursary
+
+--10
+select * from student, City
+where ((City.Name != 'Minsk' and City.Name != 'Gomel' and City.Name != 'Grodno') or City.Name = null)
+
+--11
+select * from Student
+where (Name like '%[^a-zA-Z0-9]%')
+
+--12
+select Name, Bursary from Student
+where Birthday <= '1984-04-23'
+order by GroupID, Name
+
+--13
+select Teacher.Name as Teacher, Subject.Name as Subject, Subject.Duration from Subject, Teacher
+where Teacher.SubjectID = Subject.SubjectID
+
+--14
+select "GROUP".Name as groupName, University.Name as university, City.Name as city from [dbo].[Group], University, City
+where (City.CityID = University.CityID and "Group".UniversityID = University.UniversityID)
+
+--15
+select Student.Name as StudentName, StudentSubject.Mark as Mark, Student.GroupID as "Group", City.CityID as City from StudentSubject, Student, "Group", City
+where (Mark <= 6.2 and Student.CityID = City.CityID and Student.GroupID = "Group".GroupID and Student.StudentID = StudentSubject.StudentID)
+
+--16
+select Student.Name, University.Name, City.Name, City.Population as CityPopulation from Student, University, City, "Group"
+where (City.Population > 340000 and "Group".Name like '%Uni%' and Student.CityID = City.CityID and Student.GroupID = "Group".GroupID and University.UniversityID = "Group".UniversityID)
+
+--17
+select Teacher.Name as TeacherName, UniversityTeacher.Wage, University.Name as UniName from Teacher, UniversityTeacher, University
+where (Wage >= 750 and Teacher.TeacherID = UniversityTeacher.TeacherID and University.UniversityID = UniversityTeacher.UniversityID)
+
+--18
+select Teacher.Name as TeacherName, City.Name as CityName, Subject.Name as SubjectName, UniversityTeacher.Wage, Subject.Name as SubjectName, Student.GroupID from Teacher, UniversityTeacher, Subject, Student, "Group", City
+where ((City.Name = 'Minsk' or City.Name = 'Grodno') and Subject.Name != 'English' and Teacher.SubjectID = Subject.SubjectID and Teacher.TeacherID = UniversityTeacher.TeacherID and "Group".UniversityID = UniversityTeacher.UniversityID and City.CityID = Student.CityID)
