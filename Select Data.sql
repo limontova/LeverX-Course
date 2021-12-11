80c526adbd1547357748eef2850c8b4138095a5c
limontova <62797799+limontova@users.noreply.github.com>
2021-12-11 14:21:01 +0300

    add half task Select_Data

---
+++ Select Data.sql
@@ -0,0 +1,39 @@
+USE UniversityDb;
+GO
+
+--1
+SELECT * FROM Student
+WHERE (GroupID = 101 OR GroupID = 105 OR GroupID = 106)
+
+--2
+SELECT * FROM Student
+WHERE ((GroupID = 101 OR GroupID = 105 OR GroupID = 106) AND (Bursary > 300))
+
+--3
+SELECT * FROM StudentSubject, Student
+WHERE ((Mark > 7.4 AND Mark < 9.5) AND (SUBSTRING(Name, 1, 1) = 'D'))
+
+--4
+SELECT  Name, convert(varchar, Birthday, 105) FROM Student
+WHERE (right(Name, 1) = 'O')
+
+--5
+SELECT * FROM Student
+WHERE (Bonus != NULL AND Birthday >= '1998-01-01')
+
+--6
+SELECT DISTINCT Bursary FROM Student, City
+WHERE (City.Name = 'Gomel')
+
+--7
+SELECT * FROM Student, City
+WHERE (City.Name = 'Vitebsk') ORDER BY Student.Bursary
+
+--8
+SELECT City.Name, Student.Birthday FROM Student, City
+WHERE (Birthday >= '1990-01-01' AND Birthday <= '1991-01-01') ORDER BY Bursary DESC
+
+--9
+SELECT Student.Name, Student.GroupID, Student.Bursary/max(Student.Bursary) OVER () as [BursaryPercentage] from Student
+WHERE (GroupID = 102)
+group by Student.Name, Student.GroupID, Bursary
